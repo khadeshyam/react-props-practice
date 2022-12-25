fee163b179bb998a8ed07d7f0dbd7ad2cee66a01
SHYAM KHADE <khade.shyam10.1@gmail.com>
2022-12-25 15:55:02 +0530

    add filter method to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,29 +4,27 @@ import contacts from "../contacts";
 import Avatar from "./Avatar";
 import profile from "./profile.jpg";
 
+
+function createCard(contact){
+  return(
+    <Card 
+    key ={contact.imgURL}
+    name={contact.name}
+    img={contact.imgURL}
+    tel = {contact.phone}
+    email = {contact.email}
+    />
+   )
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img = {profile} />
-      <Card 
-       name={contacts[0].name}
-       img={contacts[0].imgURL}
-       tel = {contacts[0].phone}
-       email = {contacts[0].email}
-      />
-      <Card 
-       name={contacts[1].name}
-       img={contacts[1].imgURL}
-       tel = {contacts[1].phone}
-       email = {contacts[1].email}
-      />
-      <Card 
-       name={contacts[2].name}
-       img={contacts[2].imgURL}
-       tel = {contacts[2].phone}
-       email = {contacts[2].email}
-      />
+      
+      {contacts.map(createCard)}
+  
     </div>
   );
 }
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 function Avatar(props) {
-  console.log(props);
   return (
       <img className = "circle-img"
        src={props.img}
