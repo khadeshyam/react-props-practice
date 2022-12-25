73a927b765e01c23ab24577017eaadeae2b17e5a
SHYAM KHADE <khade.shyam10.1@gmail.com>
2022-12-25 15:58:56 +0530

    completed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-import Avatar from "./Avatar";
-import profile from "./profile.jpg";
+
+
 
 
 function createCard(contact){
@@ -21,8 +21,7 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar img = {profile} />
-      
+    
       {contacts.map(createCard)}
   
     </div>
